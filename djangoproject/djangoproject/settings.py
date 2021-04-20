f074f445bfe94dc36ab1b24149ab2113d50ff6d3
johnson <kryzbone@gmail.com>
2021-04-20 14:26:35 +0000

    added environement variables

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+_dm0*5!#(izuh)(6^i$8dvm=zu$tmvb#_az8#f$z(pp4(42se'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# json.load is used to convert the string being returned by os.getenv to boolean
+DEBUG = config("DEBUG")
+
 
 ALLOWED_HOSTS = []
+
 
 
 # Application definition
