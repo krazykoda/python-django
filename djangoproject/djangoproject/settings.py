1cc8069140a4e4b6088193b4953f7f07741d140e
johnson <kryzbone@gmail.com>
2021-04-26 02:33:25 +0000

    added gunicorn

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG")
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 
@@ -127,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
